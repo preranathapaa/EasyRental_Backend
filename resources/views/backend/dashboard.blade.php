3f028628f87da67ae73700fc85ec17ff4e591d1b
preranathapaa <preranathapa890@gmail.com>
2025-05-22 20:51:08 +0545

    heading change

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -13,13 +13,13 @@
 
 
         <div class="flex flex-col p-10 bg-pink-300 flex-1  h-auto rounded-xl shadow-md shadow-amber-50">
-            <p class="text-center text-white text-7xl md:text-5xl font-extrabold">50</p>
+            <p class="text-center text-white text-7xl md:text-5xl font-extrabold">8</p>
             <p class='text-center text-white font-bold text-[20px] md:text-[16px] lg:text-[20px] '>Available Vehicles</p>
         </div>
 
 
         <div class="flex flex-col p-10 bg-orange-200 flex-1 h-auto rounded-xl shadow-md shadow-amber-50">
-            <p class="text-center text-white text-7xl md:text-5xl font-extrabold">50</p>
+            <p class="text-center text-white text-7xl md:text-5xl font-extrabold">1</p>
             <p class="text-center text-white font-bold text-[20px] md:text-[16px] lg:text-[20px]">Total Bookings</p>
         </div>
 
